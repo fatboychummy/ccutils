17897c98d43775fc10f1c52bbb40146baf620836
fatboy <fatboychummy@gmail.com>
2020-02-20 16:31:09 -0700

    Added string split function (Thanks Stack Overflow)

--- util.lua
+++ util.lua
@@ -56,6 +56,15 @@ function utils.dCopy(x)
   return ret
 end
 
-function utils.
+function funcs.split(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t={}
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
 
 return utils
